df0dfa33bcc612f899c9a3d40990678a09ac3979
Yoberson Hernandez <yobersonh98@gmail.com>
2020-08-19 22:13:29 -0400

    Commit 18/08/2020

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
-function App() {
+
+export default function App() {
   return (
-    <h2>Hola CociCurvas, pronto estarás lista :3 </h2>
+    <>
+      <Navbar />
+      <img className="main-banner__img" src="https://edteam-media.s3.amazonaws.com/specialities/original/9b01e4cc-df31-43f6-ba1e-2505ccbf641a.png" alt="banner"/>
+    </>
   );
 }
-
-export default App;
